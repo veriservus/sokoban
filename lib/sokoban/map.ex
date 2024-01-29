5c0911e3008103ad8ad148200a2b7bc9f3f7d6c1
Andrzej Byczko <veriservus@gmail.com>
2024-01-29 12:20:48 +0100

    cleaner

--- lib/sokoban/map.ex
+++ lib/sokoban/map.ex
@@ -3,7 +3,6 @@ defmodule Sokoban.Map do
 
   require Logger
 
-
   @tile_size 64
   @air '-'
   @wall '#'
@@ -30,46 +29,46 @@ defmodule Sokoban.Map do
     ]
   end
 
-  def make_tile(graph, @air, x, y) do
-    make_tile(graph, :air, x, y)
+  def make_tile(graph, @air, pos) do
+    make_tile(graph, :air, pos)
   end
 
-  def make_tile(graph, @wall, x, y) do
-    make_tile(graph, :wall, x, y)
+  def make_tile(graph, @wall, pos) do
+    make_tile(graph, :wall, pos)
   end
 
-  def make_tile(graph, @hero, x, y) do
-    make_tile(graph, :hero, x, y)
+  def make_tile(graph, @hero, pos) do
+    make_tile(graph, :hero, pos)
   end
 
-  def make_tile(graph, @hero_hole, x, y) do
-    make_tile(graph, :hero, x, y)
+  def make_tile(graph, @hero_hole, pos) do
+    make_tile(graph, :hero, pos)
   end
 
-  def make_tile(graph, @box, x, y) do
-    make_tile(graph, :box, x, y)
+  def make_tile(graph, @box, pos) do
+    make_tile(graph, :box, pos)
   end
 
-  def make_tile(graph, @box_hole, x, y) do
-    make_tile(graph, :box_hole, x, y)
+  def make_tile(graph, @box_hole, pos) do
+    make_tile(graph, :box_hole, pos)
   end
 
-  def make_tile(graph, @hole, x, y) do
-    make_tile(graph, :hole, x, y)
+  def make_tile(graph, @hole, pos) do
+    make_tile(graph, :hole, pos)
   end
 
-  def make_tile(graph, :air, _, _) do
+  def make_tile(graph, :air, _) do
     graph
   end
 
-  def make_tile(graph, fill, x, y) do
+  def make_tile(graph, fill, {x, y}) do
     rect(graph, {@tile_size, @tile_size}, fill: {:image, fill}, translate: {x*@tile_size, y*@tile_size})
   end
 
   def draw(graph) do
     {_, graph} = Enum.reduce(level1(), {{0, 0}, graph}, fn row, {{x, y}, graph} ->
-      {{_, y}, graph} = Enum.reduce(row, {{x, y}, graph}, fn tile, {{x, y}, graph} ->
-        {{x+1, y}, graph |> make_tile([tile], x, y)}
+      {{_, y}, graph} = Enum.reduce(row, {{x, y}, graph}, fn tile, {{x, y}=pos, graph} ->
+        {{x+1, y}, graph |> make_tile([tile], pos)}
       end)
       {{0, y+1}, graph}
     end)
